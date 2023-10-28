409274774068b746c8b8b7b7e954a5923675ef45
joaobaptista18 <joaobaptistadecarvalhogama@gmail.com>
2023-10-28 20:55:45 +0700

    Tugas3_1818002

---
+++ data.js
@@ -0,0 +1,127 @@
+export const CategoryList = [
+    {
+      id: 1,
+      categoryName: 'Tour',
+    },
+    {
+      id: 2,
+      categoryName: 'Custom Trip',
+    },
+    {
+      id: 3,
+      categoryName: 'Paket Wisata',
+    },
+    {
+      id: 4,
+      categoryName: 'Destinasi',
+    },
+    {
+      id: 5,
+      categoryName: 'Aktivitas',
+    },
+    {
+      id: 6,
+      categoryName: 'Tentang Kami',
+    },
+  ];
+  export const BlogList = [
+    {
+      id: 1,
+      title: 'Exploring Labuan Bajo',
+      category: 'Mount',
+      image:
+        'https://images.unsplash.com/photo-1589309736404-2e142a2acdf0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80',
+      createdAt: 'Nov 10, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 2,
+      title: 'Exploring Labuan Bajo',
+      category: 'Mount',
+      image:
+        'https://images.unsplash.com/photo-1619880938844-30735d3ea768?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80',
+      createdAt: 'Nov 10, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 3,
+      title: 'Exploring Labuan Bajo',
+      category: 'Mount',
+      image:
+        'https://images.unsplash.com/photo-1535779023901-a39d15762564?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80',
+      createdAt: 'Nov 10, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 4,
+      title: 'Exploring Labuan Bajo',
+      category: 'Mount',
+      image:
+        'https://images.unsplash.com/photo-1513601747697-f425e3d9b9ea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1738&q=80',
+      createdAt: 'Nov 10, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 5,
+      title: 'Exploring Labuan Bajo',
+      category: 'Mount',
+      image:
+        'https://images.unsplash.com/photo-1602989988967-6d763203bcbf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1520&q=80',
+      createdAt: 'Nov 10, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 6,
+      title: 'Labuan Bajo Destination',
+      category: 'Mount',
+      image:
+        'https://images.unsplash.com/photo-1602989988967-6d763203bcbf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 7,
+      title: 'Labuan Bajo Destination',
+      category: 'Mount',
+      image:
+        'https://images.unsplash.com/photo-1513601747697-f425e3d9b9ea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 8,
+      title: 'Labuan Bajo Diving',
+      category: 'Diving',
+      image:
+        'https://images.unsplash.com/photo-1499390155271-23e4667060f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fGxhYnVhbiUyMGJham8lMjBkaXZpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1756&q=80',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 9,
+      title: 'Beach',
+      category: 'Beach',
+      image:
+        'https://images.unsplash.com/photo-1536418138303-bac5f6eaa12b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 10,
+      title: 'Beach',
+      category: 'Beach',
+      image:
+        'https://images.unsplash.com/photo-1643044034131-001b53336bd0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGxhYnVhbiUyMGJham98ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1770&q=80',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 11,
+      title: 'Beach',
+      category: 'Beach',
+      image:
+        'https://images.unsplash.com/photo-1643044034131-001b53336bd0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGxhYnVhbiUyMGJham98ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1770&q=80',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+  ];
